f22003f2b3a946bed87428d40e8839ee4ccd6cf0
wadfx95174 <wadfx95174@gmail.com>
2020-09-09 00:23:41 -0700

    modify IP again

--- addr_defines.py
+++ addr_defines.py
@@ -1,12 +1,10 @@
 
 # for real experiment
-# CP_IP_eth0 = "140.116.164.141"
 # CP_IP = "192.168.2.101"
 # CP_PORT = 8001
 # TBAS_IP = "192.168.2.100"
 # TBAS_PORT = 8001
-# PI_IP_eth0 = "192.168.2.102"
-# PI_IP_eht1 = "192.168.3.102"
+# PI_IP = "192.168.2.102"
 # PI_PORT = 8001
 # CONVERTER_IP = "192.168.3.105"
 # CONVERTER_PORT = "502"
