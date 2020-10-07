35636d8864ab16fde55478433003da08b146c91b
wadfx95174 <wadfx95174@gmail.com>
2020-10-07 05:53:27 -0700

    change from fake to modbus_tk

--- verification_machine.py
+++ verification_machine.py
@@ -66,24 +66,24 @@ class ServerThread(Thread):
 
                         """ TVM send request to device for request data or control device,
                             TVM send data(with token) obtained from device to control program """
-                        # master = modbus_tcp.TcpMaster(sensorDicFromCP["converter_ip"], sensorDicFromCP["converter_port"])
-                        # try:
-                        #     responseFromDevice = master.execute(
-                        #         slave=sensorDicFromCP["slave_id"]
-                        #         , function_code=sensorDicFromCP["function_code"]
-                        #         , starting_address=sensorDicFromCP["starting_address"]
-                        #         , quantity_of_x=sensorDicFromCP["quantity_of_x"])
-                        #     print(responseFromDevice)
+                        master = modbus_tcp.TcpMaster(sensorDicFromCP["converter_ip"], sensorDicFromCP["converter_port"])
+                        try:
+                            responseFromDevice = master.execute(
+                                slave=sensorDicFromCP["slave_id"]
+                                , function_code=sensorDicFromCP["function_code"]
+                                , starting_address=sensorDicFromCP["starting_address"]
+                                , quantity_of_x=sensorDicFromCP["quantity_of_x"])
+                            # print(responseFromDevice)
 
-                        # except modbus_tk.modbus.ModbusError as exc:
-                        #     print("%s- Code=%d", exc, exc.get_exception_code())
-                        #     self._conn.close()
-                        #     print(self._addr, "disconnect!")
+                        except modbus_tk.modbus.ModbusError as exc:
+                            print("%s- Code=%d", exc, exc.get_exception_code())
+                            self._conn.close()
+                            print(self._addr, "disconnect!")
 
-                        # except modbus_tcp.ModbusInvalidMbapError as exc:
-                        #     print(exc)
-                        #     self._conn.close()
-                        #     print(self._addr, "disconnect!")
+                        except modbus_tcp.ModbusInvalidMbapError as exc:
+                            print(exc)
+                            self._conn.close()
+                            print(self._addr, "disconnect!")
 
                         global jwtFromTTAS_TVM
                         # verify jwt from TVM via signature and decode it via rsa's public key
@@ -104,8 +104,8 @@ class ServerThread(Thread):
                             except jwt.InvalidAudienceError:
                                 connectTTAS()
                         
-                        # response = (jwtFromTTAS_TVM.decode("utf-8") + "+++++" + json.dumps(responseFromDevice)).encode("utf-8")
-                        response = (jwtFromTTAS_TVM.decode("utf-8") + "+++++" + json.dumps((1234, 2234, 3234))).encode("utf-8")
+                        response = (jwtFromTTAS_TVM.decode("utf-8") + "+++++" + json.dumps(responseFromDevice)).encode("utf-8")
+                        # response = (jwtFromTTAS_TVM.decode("utf-8") + "+++++" + json.dumps((1234, 2234, 3234))).encode("utf-8")
                         # print("response", response)
                         # time.sleep(12)
                         self._conn.sendall(response)
@@ -126,8 +126,8 @@ class ServerThread(Thread):
                             else:
                                 print(feadbackFromCP)
                                 connectTTAS()
-                                # response = (jwtFromTTAS_TVM.decode("utf-8") + "+++++" + json.dumps(responseFromDevice)).encode("utf-8")
-                                response = (jwtFromTTAS_TVM.decode("utf-8") + "+++++" + json.dumps((1234, 2234, 3234))).encode("utf-8")
+                                response = (jwtFromTTAS_TVM.decode("utf-8") + "+++++" + json.dumps(responseFromDevice)).encode("utf-8")
+                                # response = (jwtFromTTAS_TVM.decode("utf-8") + "+++++" + json.dumps((1234, 2234, 3234))).encode("utf-8")
                                 self._conn.sendall(response)
                                 feadbackFromCP = self._conn.recv(1024).decode("utf-8")
 
